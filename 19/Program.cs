b7283f51128e765bba71ca2f9c978760002cdea0
Nataly <17pn@mail.ru>
2022-07-07 09:19:41 +0300

    домашнее задание 3

--- 19/Program.cs
+++ 19/Program.cs
@@ -4,27 +4,29 @@
 12821 -> да
 23432 -> да*/
 
-/*bool isPalindromLiht(int[] arr) 
-{
-    int result = false;
-    const N = arr.Length;
-    for (int i = 0; i < N / 2; i++)
-    {
-        if (arr[i] !== arr[N-i-1])
-        {
+/*bool isPalindromLiht(int[] arr) {
+    bool result = false;
+    const N = arr.Lenght;
+    for (int i = 0; i < N / 2; i++) {
+        if (arr[i] !== arr[N-i-1])  {
             Console.WriteLine("Не палиндром");
             return result;
         }
     }
-    int result = true;
+    result = true;
     Console.WriteLine("Число - палиндром");
+    
     return result;
 }
 
-isPalindromLiht[12345];*/
+isPalindromLiht(12345);*/
 
-bool isPalindrom(int x) {
+/*bool isPalindrom(int x) {
     int div = 1;
+    while (x / div >= 10) {
+        div *= 10;
+    }
+    Console.WriteLine(div);
     while (x != 0) {
         int l = x / div;
         int r = x % 10:
@@ -39,4 +41,29 @@ bool isPalindrom(int x) {
     return true;
 } 
 
-isPalindrom(12345);
+isPalindrom(12345);*/
+
+
+class Program {  
+    static void Main(string[] args) {  
+        int num, rem, sum = 0, temp;      
+        Console.WriteLine("Определение является ли число палиндромом");  
+        Console.Write("Введите число: ");  
+        num = Convert.ToInt32(Console.ReadLine());  
+        temp = num;  
+        while (num > 0) {  
+            rem = num % 10;     
+            num = num / 10;    
+            sum = sum * 10 + rem;   
+        }  
+        Console.WriteLine("Перевернутое число: {0} ", sum);  
+        if (temp == sum) 
+        {  
+            Console.WriteLine("Это число палиндром");  
+        } else {  
+            Console.WriteLine("Это число не палиндром");  
+        }  
+        Console.ReadLine();  
+    }  
+}    
+  
